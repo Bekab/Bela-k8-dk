e81cbce7d26ce05bacd3c49427b83fd79eeda53d
Bekab <bereketabmamo@gmail.com>
2025-05-15 14:23:52 +0000

    new RG

--- Iac/cont.regi/variables.tf
+++ Iac/cont.regi/variables.tf
@@ -1,13 +1,13 @@
 variable "resource_group_name" {
   description = "The name of the resource group"
   type        = string
-  default = "k-8dkRG"
+  default = "abelk8"
 }
 
 variable "location" {
   description = "The Azure location for resources"
   type        = string
-  default     = "East US"
+  default     = "Poland Central"
 }
 
 variable "container_registry_name" {
