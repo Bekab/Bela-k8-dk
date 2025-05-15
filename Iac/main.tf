3dffb7ca87ec6bd5fccdcee5616a21df7e58392f
Bekab <bereketabmamo@gmail.com>
2025-05-15 13:50:50 +0000

    add-ucliiiii

--- Iac/main.tf
+++ Iac/main.tf
@@ -2,6 +2,7 @@ module "container_registry" {
   source = "./cont.regi"
   providers = {
     azurerm = azurerm.azresourceprovider
+     use_cli = true
   }
 
 }
@@ -10,6 +11,7 @@ module "web_app" {
   source = "./web-app"
   providers = {
     azurerm = azurerm.azresourceprovider
+     use_cli = true
   }
 
 }
