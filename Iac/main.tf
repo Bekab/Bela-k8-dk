e5ee2a1fae93c9f30b206baeb6060e50df829ff4
Bekab <bereketabmamo@gmail.com>
2025-05-15 13:54:13 +0000

    provider+module--main

--- Iac/main.tf
+++ Iac/main.tf
@@ -1,17 +1,10 @@
 module "container_registry" {
   source = "./cont.regi"
-  providers = {
-    azurerm = azurerm.azresourceprovider
-     use_cli = true
-  }
+
 
 }
 
 module "web_app" {
   source = "./web-app"
-  providers = {
-    azurerm = azurerm.azresourceprovider
-     use_cli = true
-  }
 
 }
--- Iac/web-app/main.tf
+++ Iac/web-app/main.tf
@@ -1,3 +1,10 @@
+# provider.tf
+provider "azurerm" {
+  features {}
+  alias           = "azresourceprovider"
+    use_cli = true
+}
+
 resource "azurerm_resource_group" "main" {
   name     = var.resource_group_name
   location = var.location
